c8da9d1c59868cb556f4f981c908d34e5dfdc907
Volodymyr Pomirko <volodymyr.pomirko@gmail.com>
2024-11-05 05:06:04 +0200

    added lab4 with Butterfly class

--- src/lab4.py
+++ src/lab4.py
@@ -21,9 +21,6 @@ class Clip:
             return self.__views
 
 
-    def __str__(self):
-            return f"{self.__artist}, {self.__song}, {self.__duration}, {self.__views}, {self.genre}, {self.awards}"
-
     def __repr__(self):
         return (
             f"Clip:\n"
@@ -35,6 +32,11 @@ class Clip:
             f"  Awards: {self.awards}"
 
         )
+
+
+    def __str__(self):
+            return f"{self.__artist}, {self.__song}, {self.__duration}, {self.__views}, {self.genre}, {self.awards}"
+
 
     def __del__(self):
         print(f"clip '{self.__song}' was deleted.")
@@ -53,6 +55,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
